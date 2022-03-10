1ae7d5e0daac78047a1f092425920df48c964fd6
Philip Freude <philipfreude@gmail.com>
2022-03-10 21:56:41 +0100

    Upgrade nodejs version for homebridge to 16.x

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #   include homebridge
 class homebridge (
   String  $version                = 'latest',
-  String  $nodejs_repo_url_suffix = '14.x',
+  String  $nodejs_repo_url_suffix = '16.x',
   String  $ui_ensure              = 'installed',
   Boolean $ui_enabled             = true,
 ) {
